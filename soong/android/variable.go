d4a65a7bd27eb14ed92ae5ab47b3930ead03ce4c
Nick Reuter <nreuter85@gmail.com>
2017-08-22 18:11:35 -0500

    soong: add Has_legacy_mmap product variable
    Change-Id: I743eeac6ec40e356fdeb2e8c4f8957c09e84b434

--- soong/android/variable.go
+++ soong/android/variable.go
@@ -11,10 +11,15 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+
+	Has_legacy_mmap struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Has_legacy_mmap  *bool `json:",omitempty"`
 }
